123c87abefbae4373445af01e9c182903bd25bd2
harry <harrison@1bg>
2015-07-26 19:29:36 +0000

    form update

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "harrisonwallin@gmail.com";
+    $subject = "Portfolio email";
+    $headers = "From: " . $name . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $email_body = $message;
+
+    mail($to,$subject,$email_body,$headers);
+
+?>
